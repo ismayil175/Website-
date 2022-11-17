d907102de747a39da89f40c0589d5da5e4b0fd1a
ISMAYIL MAHARRAMOV <114576055+ismayil175@users.noreply.github.com>
2022-11-17 10:57:44 +0200

    Add files via upload

---
+++ function.js
@@ -0,0 +1,36 @@
+function person() {
+    alert("This website was developed by Maksiym Laptin");
+}
+
+function calculator()
+    {
+        var num1 = window.prompt("Enter a Number");
+        var num2 = window.prompt("Enter second number");
+
+        num1 = parseInt(num1);
+        num2 = parseInt(num2);
+        num3 = num1+num2;
+        alert("The result is: " + num3);
+    }
+
+    
+
+
+	//Validtion Code For Inputs
+
+    function auth(){
+        var name = document.getElementById("name").value;
+        var sname = document.getElementById("password").value;
+        if(name=="max" && sname=="max"){
+          
+          window.location.href = 'features.html' ;
+          alert("Success!!");
+          
+        }
+        else{
+          alert("Wrong Name or Surname");
+          
+        }
+      }
+
+      
